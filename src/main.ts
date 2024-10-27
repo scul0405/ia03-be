63791a6537cfc649a421bd4e9d986540c1f3b7c5
Vi Lý Duy Trường <90521402+scul0405@users.noreply.github.com>
2024-10-28 00:20:38 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // Frontend URL
+    origin: 'https://ia03-user-registration-21120353.netlify.app', // Frontend URL
     credentials: true,
   });
 
