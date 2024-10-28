8399d074048c9b64171bc373916c3bfb6edd4019
Vi Lý Duy Trường <90521402+scul0405@users.noreply.github.com>
2024-10-28 23:52:08 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,12 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: /.+/ });
+  
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://ia03-user-registration-21120353.netlify.app');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 
   await mongoose.connect(process.env.DATABASE_URI);
 
